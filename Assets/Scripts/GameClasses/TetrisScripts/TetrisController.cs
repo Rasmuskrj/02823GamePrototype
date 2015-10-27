07f1ae66f39c8e8a04a0a07fcc60b893c6da6b1b
Rasmus Krøyer Jørgensen <rasmuskrj@gmail.com>
2015-10-27 15:21:33 +0100

    initial Tetris commit

---
+++ Assets/Scripts/GameClasses/TetrisScripts/TetrisController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TetrisController : MonoBehaviour {
+
+    private float unitDistance = 1.0f;
+    public GameObject leftWall;
+    public GameObject rightWall;
+
+    public static TetrisController Instance
+    {
+        get;
+        private set;
+    }
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+	// Use this for initialization
+	void Start () {
+        Vector3 pointLeft = leftWall.GetComponent<Collider>().ClosestPointOnBounds(rightWall.transform.position);
+        Vector3 pointRight = rightWall.GetComponent<Collider>().ClosestPointOnBounds(leftWall.transform.position);
+
+        float distance = Vector3.Distance(pointLeft, pointRight);
+        unitDistance = distance / 10.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public float GetUnitDistance()
+    {
+        return unitDistance;
+    }
+}
