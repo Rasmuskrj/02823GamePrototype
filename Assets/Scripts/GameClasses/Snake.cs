37414223632566bac910c42d0d6df315a49fb717
Pavel <kasiclysm+git@gmail.com>
2015-11-10 15:38:12 +0100

    Snake movements for controller

--- Assets/Scripts/GameClasses/Snake.cs
+++ Assets/Scripts/GameClasses/Snake.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class Snake : MonoBehaviour, IGameTypeInterface
 {
-
+    public Vector2 dir = Vector2.right;
+    public SnakeHead snakeHead;
 	// Use this for initialization
 	void Start () {
 	
@@ -16,9 +17,19 @@ public class Snake : MonoBehaviour, IGameTypeInterface
     public void MoveX(float axisx)
     {
 
-    }
+        if (axisx > 0.5f && dir != Vector2.left)
+            dir = Vector2.right;
+        else if (axisx<0.5f && dir != Vector2.right)
+            dir = -Vector2.right; // '-right' means 'left'
+        snakeHead.SetDir(dir);
+        }
     public void MoveY(float axisy)
     {
+        if (axisy > 0.5f && dir != -Vector2.up)
+            dir = Vector2.up;
+        else if (axisy<0.5f && dir != Vector2.up)
+            dir = -Vector2.up;    // '-up' means 'down'
+        snakeHead.SetDir(dir);
 
     }
 }
