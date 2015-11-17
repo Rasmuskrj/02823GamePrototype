54fe68a9acdbb5f638ed9d1335a7a7e4dec9096d
Emil Gensby <Emil Gensby>
2015-11-17 16:09:19 +0100

    shooter updated

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+    public GameObject enemy;
+    public Transform enemySpawnLeft;
+    public Transform enemySpawnRight;
+    public Transform enemySpawnTop;
+    public Transform enemySpawnBottom;
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/GameClasses/SurvivalScripts/PlayerHealth.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startingHealth = 3;   
+    public int currentHealth;
+                
+    bool isDead;   
+    bool damaged;         
+
+
+    void Awake()
+    {
+        currentHealth = startingHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+
+        if (currentHealth <= 0 && !isDead)
+        {
+            
+        }
+    }
+
+
+    void Death()
+    {
+        isDead = true;
+        //disable game somehow?
+    }
+}
