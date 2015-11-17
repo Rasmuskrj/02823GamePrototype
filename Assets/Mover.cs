ae24cfc7be65bc9c6a3cbb59f4209a801b32cb3e
Emil Gensby <Emil Gensby>
2015-11-17 15:03:01 +0100

    initial survival game commit

---
+++ Assets/Mover.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+
+    public float speed;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity *= speed;
+    }
+
+    void Update()
+    {
+        if (Mathf.Abs(transform.localPosition.x) > 30f)
+        {
+            Destroy(gameObject);
+        }
+
+        if (Mathf.Abs(transform.localPosition.y) > 30f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Destroy(col.gameObject);
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public GameObject shot;
+    public Transform shotSpawn;
+
+    public float fireRate;
+    private bool p1x_isAxisInUse = false;
+    private bool p1y_isAxisInUse = false;
+    private float nextFire;
+
+    void Update()
+    {
+        if (Input.GetAxisRaw("P1Horizontal") != 0)
+        {
+            if (p1x_isAxisInUse == false)
+            {
+                p1x_isAxisInUse = true;
+                MoveXRaw(Input.GetAxisRaw("P1Horizontal"));
+            }
+        }
+        else
+        {
+            p1x_isAxisInUse = false;
+        }
+
+        if (Input.GetAxisRaw("P1Vertical") != 0)
+        {
+            if (p1y_isAxisInUse == false)
+            {
+                p1y_isAxisInUse = true;
+                MoveYRaw(Input.GetAxisRaw("P1Vertical"));
+            }
+        }
+        else
+        {
+            p1y_isAxisInUse = false;
+        }
+    }
+
+    public void MoveXRaw(float axisx)
+    {
+        GameObject newShot;
+        Rigidbody rb;
+        newShot = Instantiate(shot);
+        rb = newShot.GetComponent<Rigidbody>();
+        newShot.transform.parent = gameObject.transform;
+        rb.velocity = new Vector3(axisx, 0, 0);
+    }
+
+    public void MoveYRaw(float axisy)
+    {
+        GameObject newShot;
+        Rigidbody rb;
+        newShot = Instantiate(shot);
+        rb = newShot.GetComponent<Rigidbody>();
+        newShot.transform.parent = gameObject.transform;
+        rb.velocity = new Vector3(0, axisy, 0);
+    }
+
+
+}
