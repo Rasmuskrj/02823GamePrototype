3a5981fd3f68b643c691f1c7a31a372ad45c39da
neroes <sorenjesperbloch@msn.com>
2015-12-01 13:44:12 +0100

    added target keys

--- Assets/Scripts/GameClasses/SurvivalScripts/PlayerController.cs
+++ Assets/Scripts/GameClasses/SurvivalScripts/PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : MonoBehaviour
     public Transform shotSpawn;
 
     public float fireRate;
-    private float nextFire;
+    private float nextFire = 0;
     
 
     void Update()
@@ -17,13 +17,18 @@ public class PlayerController : MonoBehaviour
 
     public void MoveXRaw(float axisx)
     {
-        GameObject newShot;
-        Rigidbody rb;
-        newShot = Instantiate(shot);
-        rb = newShot.GetComponent<Rigidbody>();
-        newShot.transform.parent = gameObject.transform;
-        newShot.transform.localPosition = new Vector3(0f, 0f, 0f);
-        rb.velocity = new Vector3(axisx, 0, 0);
+        if (Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            GameObject newShot;
+            Rigidbody rb;
+            newShot = Instantiate(shot);
+            rb = newShot.GetComponent<Rigidbody>();
+            newShot.transform.parent = gameObject.transform;
+            newShot.transform.localPosition = new Vector3(0f, 0f, 0f);
+            rb.velocity = new Vector3(axisx, 0, 0);
+        }
+        
     }
 
     public void MoveYRaw(float axisy)
